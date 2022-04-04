e60879d48b356d4559168ef38a9b301948667ee1
Ibrahim AlDekhail <33824044+idekhail@users.noreply.github.com>
2022-04-05 01:49:49 +0300

    Add files via upload
    you need to open NuGet packages  then download
    mysql.data     version      8.0.28
    system.data.sqlclient      version     4.8.3

---
+++ MainPage.xaml.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+namespace XF_SqlServer
+{
+    public partial class MainPage : ContentPage
+    {
+        SqlConnection sqlConnection;
+        public MainPage()
+        {
+            InitializeComponent();
+            // Local Server
+            string serverdbname = "mydb";
+            string servername = "192.168.0.104";
+            string serverusername = "student";
+            string serverpassword = "Ss@123";
+            // Online Server
+            //string serverdbname = "db_a6410c_mydb";
+            //string servername = "SQL5104.site4now.net";
+            //string serverusername = "db_a6410c_mydb_admin";
+            //string serverpassword = "Ss@123456";
+
+            string sqlConn = $"Data Source={servername};Initial Catalog={serverdbname};User Id={serverusername};Password={serverpassword}";
+
+           // string sqlConn = $"Data Source=SQL5104.site4now.net;Initial Catalog=db_a6410c_mydb;User Id=db_a6410c_mydb_admin;Password=Ss@123456";
+
+            sqlConnection = new SqlConnection(sqlConn);
+        }
+        private async void TestConnection_Clicked(object sender, System.EventArgs e)
+        {
+            try
+            {
+                sqlConnection.Open();
+                await DisplayAlert("Connection", "Connection Established", "Ok"); 
+                sqlConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error Connection", ex.Message, "Ok");
+                throw;
+            }
+        }
+        private async void GetInfo_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                List<MyTable> myList = new List<MyTable>();
+                sqlConnection.Open();
+                string queryString = "SELECT * FROM dbo.mytable";
+                SqlCommand command = new SqlCommand(queryString, sqlConnection);
+                SqlDataReader reader = command.ExecuteReader();                  
+                while (reader.Read())
+                {
+                    myList.Add(new MyTable
+                    {
+                        Id = Convert.ToInt32(reader["Id"]),
+                        Title = reader["Title"].ToString(),
+                        Body = reader["Body"].ToString(),
+                    });
+                }
+                reader.Close();
+                sqlConnection.Close();
+                MyListView.ItemsSource = myList;
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error Connection", ex.Message, "Ok");
+                throw;
+            }
+        }
+        private async void InsertInfo_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(UserId.Text) && !string.IsNullOrEmpty(UserTitle.Text)
+                    && !string.IsNullOrEmpty(UserBody.Text))
+                {
+                    sqlConnection.Open();
+                    using (SqlCommand command = new SqlCommand("INSERT INTO dbo.mytable VALUES(@Id, @Title, @Body)", sqlConnection))
+                    {
+                        command.Parameters.Add(new SqlParameter("Id", UserId.Text));
+                        command.Parameters.Add(new SqlParameter("Title", UserTitle.Text));
+                        command.Parameters.Add(new SqlParameter("Body", UserBody.Text));
+                        command.ExecuteNonQuery();
+                    }
+                    sqlConnection.Close();
+                    await DisplayAlert("Insert", "Insert Data", "Ok");
+                }
+                else
+                    await DisplayAlert("Null", "Empty" , "Ok");
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error", ex.Message, "Ok");                
+            }
+        }
+        private async void UpdateInfo_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(UserId.Text) && !string.IsNullOrEmpty(UserTitle.Text)
+                    && !string.IsNullOrEmpty(UserBody.Text))
+                {
+                    sqlConnection.Open();
+                    int UId = Convert.ToInt32(UserId.Text);
+                    string UTitle = UserTitle.Text;
+                    string UBody = UserBody.Text;
+
+                    string queryS = $"UPDATE dbo.mytable SET Id='{UId}',Title='{UTitle}',Body='{UBody}' WHERE Id='{UId}'";
+                    using(SqlCommand command = new SqlCommand(queryS, sqlConnection))
+                    {
+                        command.ExecuteNonQuery();
+                    }
+                    sqlConnection.Close();
+                    await DisplayAlert("Update", "Updated", "Ok");
+                }
+                else
+                    await DisplayAlert("Null", "Empty", "Ok");
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error", ex.Message, "Ok");
+                throw;
+            }
+        }
+        private async void DeleteInfo_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(UserId.Text))
+                {
+                    sqlConnection.Open();
+                    int UId = Convert.ToInt32(UserId.Text);
+                
+                    using (SqlCommand command = new SqlCommand($"DELETE FROM dbo.mytable WHERE Id='{UId}'", sqlConnection))
+                    {
+                        command.ExecuteNonQuery();
+                    }
+                    sqlConnection.Close();
+
+                    await DisplayAlert("Delete", "Deleted", "Ok");
+                }
+                else
+                    await DisplayAlert("Null", "Empty", "Ok");
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error", ex.Message, "Ok");
+                throw;
+            }
+        }           
+        private void Clear_Clicked(object sender, EventArgs e)
+        {
+            UserId.Text = ""; UserTitle.Text = "";UserBody.Text = "";
+        }
+        public class MyTable
+        {
+            public int Id { get; set; }
+            public string Title { get; set; }
+            public string Body { get; set; }
+        }       
+    }
+}
---
+++ MySqlPage.xaml.cs
@@ -0,0 +1,165 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XF_SqlServer
+{
+    public partial class MySqlPage : ContentPage
+    {
+        MySqlConnection connection;
+        public MySqlPage()
+        {
+            InitializeComponent();
+            // Local Server
+            //string serverdbname = "mydb";
+            //string servername = "192.168.0.104";
+            //string serverusername = "student";
+            //string serverpassword = "Ss@123";
+            //string serverport = "3306";          
+
+            //  string sqlConn = $"Server={servername};Port={serverport};database={serverdbname};User Id={serverusername};Password={serverpassword};charset=utf8";
+            string sqlConn = "Server=MYSQL5025.site4now.net;Database=db_a6410c_student;Uid=a6410c_student;Pwd=Ss@123456";
+
+            connection = new MySqlConnection(sqlConn);
+        }
+        private async void TestConnection_Clicked(object sender, System.EventArgs e)
+        {
+            if (connection.State == ConnectionState.Closed)
+            {
+                try
+                {
+                    connection.Open();
+                    await DisplayAlert("Yeah", "Connection established", "OK");
+
+                }
+                catch (MySqlException ex)
+                {
+                    await DisplayAlert("Naaah", "Connection failed: " + ex.Message, "Quit");
+                }
+                finally
+                {
+
+                    connection.Close();
+                    await DisplayAlert("Finally", "Connection closed", "OK");
+                }
+            }
+        }
+        private async void GetInfo_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                List<MyTable> myList = new List<MyTable>();
+
+                connection.Open();
+
+                var command = connection.CreateCommand();
+                command.CommandText = "SELECT * FROM mytable";
+
+                var reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    myList.Add(new MyTable
+                    {
+                        Id = Convert.ToInt32(reader["Id"]),
+                        Title = reader["Title"].ToString(),
+                        Body = reader["Body"].ToString(),
+                    });
+                }
+
+                reader.Close();
+                connection.Close();
+                MyListView.ItemsSource = myList;
+
+            }catch (Exception ex)
+            {
+                await DisplayAlert("Error Connection", ex.Message, "Ok");              
+            }
+        }
+        private async void InsertInfo_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(UserId.Text) && !string.IsNullOrEmpty(UserTitle.Text)
+                    && !string.IsNullOrEmpty(UserBody.Text))
+                {
+                    connection.Open();
+                    MySqlCommand command = new MySqlCommand("INSERT INTO mytable (Id, Title, Body) VALUES('" + UserId.Text + "','" + UserTitle.Text + "','" + UserBody.Text + "')", connection);
+                    var reader = command.ExecuteReaderAsync();
+                    connection.Close();
+                    await DisplayAlert("Insert", "Insert Data", "Ok");
+                }
+                else
+                    await DisplayAlert("Info", "Empty", "Ok");
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error", ex.Message, "Ok");
+            }
+        }
+
+        private async void UpdateInfo_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(UserId.Text) && !string.IsNullOrEmpty(UserTitle.Text)
+                    && !string.IsNullOrEmpty(UserBody.Text))
+                {
+                    connection.Open();
+                    int UId = Convert.ToInt32(UserId.Text);
+                    string UTitle = UserTitle.Text;
+                    string UBody = UserBody.Text;
+
+                    string queryS = $"UPDATE mytable SET Id='{UId}',Title='{UTitle}',Body='{UBody}' WHERE Id='{UId}'";
+                    MySqlCommand command = new MySqlCommand(queryS, connection);
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                    await DisplayAlert("Update", "Updated", "Ok");
+                }
+                else
+                    await DisplayAlert("Null", "Empty", "Ok");
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error", ex.Message, "Ok");
+                throw;
+            }
+        }
+        private async  void DeleteInfo_Clicked(object sender, EventArgs e)
+    {
+            try
+            {
+                if (!string.IsNullOrEmpty(UserId.Text))
+                {
+                    connection.Open();
+                    int UId = Convert.ToInt32(UserId.Text);
+
+                    MySqlCommand command = new MySqlCommand($"DELETE FROM mytable WHERE Id='{UId}'", connection);
+                    command.ExecuteNonQuery();
+                    connection.Close();
+
+                    await DisplayAlert("Delete", "Deleted", "Ok");
+                }
+                else
+                    await DisplayAlert("Null", "Empty", "Ok");
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error", ex.Message, "Ok");
+                throw;
+            }
+        }
+        private void Clear_Clicked(object sender, EventArgs e)
+        {
+            UserId.Text = ""; UserTitle.Text = ""; UserBody.Text = "";
+        }
+        public class MyTable
+        {
+            public int Id { get; set; }
+            public string Title { get; set; }
+            public string Body { get; set; }
+        }
+    }
+}
